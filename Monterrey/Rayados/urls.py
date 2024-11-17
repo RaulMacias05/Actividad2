fea7d8ec70ee22dcceee95a1c14e9ea797b52c4d
Raúl Macías <raul.sanchezm@uanl.edu.mx>
2024-11-16 21:01:05 -0600

    Se agrega al configuración de los templates para Rayados (con errores)

---
+++ Monterrey/Rayados/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from Rayados import views
+
+urlpatterns = [
+    path("",views.index, name="index.html"),
+    path("contacto.html/",views.contacto, name="contacto.html"),
+    path("estadio.html/",views.estadio, name="estadio.html"),
+    path("jugadores.html/",views.jugadores, name="jugadores.html"),
+    path("titulos.html/",views.titulos, name="titulos.html")
+]
--- Monterrey/Rayados/views.py
+++ Monterrey/Rayados/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "Rayados/index.html", {})
+
+def contacto(request):
+    return render(request, "Rayados/contacto.html", {})
+
+def estadio(request):
+    return render(request, "Rayados/estadio.html", {})
+
+def jugadores(request):
+    return render(request, "Rayados/jugadores.html", {})
+
+def titulos(request):
+    return render(request, "Rayados/titulos.html", {})
